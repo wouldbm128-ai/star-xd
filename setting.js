5d5aa68c7eb72a53535a21b7e6ae9941d8636e3d
wouldbm128-ai <wouldbm128@gmail.com>
2026-02-19 16:46:55 +0200

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,13 +1,14 @@
 //=====💫 sᴛᴀʀ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'Leader accounts', // ur bot name
+  OWNER_NAME: 'Leader accounts', //your name
+  OWNER_NUMBER: '201105274900', // add your phone number
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
+  SESSION_ID: 'KnightBot!H4sIAAAAAAAAA5VUW5OiOBj9K1t5xRq5KApVXbVA24Jie0XFrXmIECDKNQkgTPnft7C7p+dhd7b3LSThfOc753z5AdIMUzRHDVB/gJzgCjLULVmTI6ACvQwCREAP+JBBoIJsCvs6qU9LYt/W0VDfRulEydpFxvM8J2hhPLsoSTqqd0ftCdx7IC/PMfZ+Axiv0/qwKgtWnf0Zkjlvae/tiZDUYSy+UkN7LtogfTaKneg8gXuHCDHBaTjJI5QgAuM5alYQk6/Rt+wMCqW+upziqz4PlnPZZcN4mVT60TmJ1TxKh7WUvMA4pV+j7x4CpS9e4Jothvxsa2Y+UiQdNfzh1J6DYKAVq4MMj9les97oUxymyLd8lDLMmi/rDq111R54zhDiK8XcbjG5KFDXFGO0PC1NudhNY4GOLs9M+qLuZy7hnGVVO7y0uRUxf5Omx8Jdipycadn0mdghr5nmxrgk9a/EV+QjK9f/o7s2cUQtM2augE5jTb6KprtO/EoXRHfv7OJjMqsaLOXTDT/5Gv2Gk/x1wCHZLlKtP1p7I7nlGv9irPx+7Jt0JxRx49nn/YvzSR+ykvyOZTmxbXitU9cZviQGtufVeWHcLkp/d55Hi2uwW/Zbtzww6mZK2xaxO7OjhBsUI+w4+/GZrS/48LIK3NVpW5x26wjZAx7XT4+OrqixfKAK9x4gKMSUEchwlj72ZL4HoF9tkUcQe8gLjvJaIntPWiqvx3zQBFmItWPWXpWIKNbrLvbKbQnz8Dqzrk+gB3KSeYhS5JuYsow0C0QpDBEF6l/feyBFN/ZmXFdOEnogwIQyJy3zOIP+h6sfh9DzsjJl2yb1jG6BCFD5z23EGE5D2ulYppB4Ea6QEUFGgRrAmKKfHSKCfKAyUqKfU2tkfif8QXzer4TREPRA8jAE+0AFw/FAlAcDeTCQhqog/km/1R0szPNvKWKgB1LY3QY2gj4if7zzAT0QP34XhgN+LMq8LPDjkSR0CN3B/SfzrpCPGMQxBSowFs3wtnLNybyB0qKeTrVJqBmhBj47/YjMmyW6smtWRzKm52wy0IvDpKAT/GozSvWmsfkTGaCGn0+94XHx9A8gQAWbEJoaRzejIJCDW3/VOC5+vS3NdrMwr40RKiTQ0n5fIGvsocNzyCrBmYYy9X1+WhYROwx4NouJyEm2Yoh6uxrXhaE9ddV8VGEP/Vpsyy3kooL7KD8G4+vLplT2s0v94kRnhV+eRrWALZFWGoGN77Qx2RZtkg+Ru6ksJhKmbU33OBcXNdt5p9ycmdPIPpoX7S3Mj2GK3x8x/IhZ52H3GWD0eBPevfpPT9+Id9Hj771fMN5fmX+ZVH0dOOOLIPftqawvNRhHc6Jbl+3obGsexcoUciZlRpQu9y2437/3QB5DFmQkASqgyRmCHiBZ2QXZSoPsN5UMzbL0dWh1bceQMu1zOHY4QZTBJAeqMBoJQ15WFOHt1opkuQlpBFSQJpJ+Bve/AbVoi5ZpBwAA
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɴɪɢʜᴛ ʙᴏᴛ*', // add your session ID
   PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
